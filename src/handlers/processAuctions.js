dd7e95907d0cdeacdde6f35159a56b57f89f9887
javahyang <javahyang@gmail.com>
2021-01-13 21:04:53 +0900

    38. Promise.all 적용해 auctions 닫기

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,19 @@
+import createError from 'http-errors';
+import { closeAuction } from '../lib/closeAuction';
 import { getEndedAuctions } from '../lib/getEndedAuctions';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
+
+    // 이 함수는 api gateway를 통해 트리거되는게 아니기 때문에, 원하는대로 리턴값을 지정할 수 있다
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
