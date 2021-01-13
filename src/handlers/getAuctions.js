4b1a0ee9b0111eb5c14320e3400bc5d5cf9da22d
javahyang <javahyang@gmail.com>
2021-01-13 22:34:24 +0900

    40. scan -> query 변경

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -5,12 +5,23 @@ import createError from 'http-errors';
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
   try {
-    const reseult = await dynamodb.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-    }).promise();
+    const reseult = await dynamodb.query(params).promise();
 
     auctions = reseult.Items;
   } catch (error) {
