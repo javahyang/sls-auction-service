08601cd03cc2ab55931b9743dce059ffd2737fc9
javahyang <javahyang@gmail.com>
2021-01-14 23:25:56 +0900

    64. challenge _ my version

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,6 +16,30 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  await dynamodb.update(params).promise();
+  
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  // 입찰자가 없을 때 셀러에게 메시지 다르게
+
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'Your item has been sold!',
+      recipient: seller,
+      body: bidder ? `Woohoo! Your item "${title}" has been sold for $${amount}.` : `No body bid on your item`,
+    })
+  }).promise();
+
+  const notifyBidder = bidder ? sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'You won an auction!',
+      recipient: bidder,
+      body: `What a great deal! You got yourself a "${title}" for $${amount}.`,
+    })
+  }).promise() : true;
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
